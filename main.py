eabba591929dee67e41d87f2706abced6b6c09f7
Tomiyasu <rtommy1016@gmail.com>
2024-12-31 01:24:42 +0900

    login

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
-
-#ユーザーID、パスワード、および役職の辞書（役職情報を追加）
-USER_CREDENTIALS = {
-    "Tomiyasu": {"password": "First", "role": "部長"},
-    "Ryo": {"password": "Last", "role": "課長"},
-    "user3": {"password": "Pass123", "role": "GL"},
-}
+import requests
+import pandas as pd
+# ユーザーID、パスワード、および役職の辞書（役職情報を追加）
+#USER_CREDENTIALS = {
+    #"Tomiyasu": {"password": "First", "role": "部長"},
+    #"Ryo": {"password": "Last", "role": "課長"},
+    #"user3": {"password": "Pass123", "role": "GL"},
+#}
 
 def login():
     st.title("ログイン画面")
@@ -13,19 +14,27 @@ def login():
     # ユーザーIDとパスワードの入力欄
     username = st.text_input("ユーザーID")
     password = st.text_input("パスワード", type="password")
+    
+
+    
 
     # ログインボタン
     if st.button("ログイン"):
-        if username in USER_CREDENTIALS and USER_CREDENTIALS[username]["password"] == password:
+        url = "https://altxfastapi-test.onrender.com/login/"
+        res = requests.get(url + username + "/" +password)
+        data = res.json()
+        if data != False:
             st.session_state["authenticated"] = True
             st.session_state["username"] = username
-            st.session_state["role"] = USER_CREDENTIALS[username]["role"]
+            st.session_state["role"] = data
+            print(data)
             st.success(f"ログイン成功: ようこそ {username} さん！")
             st.rerun()
         else:
             st.error("ユーザーIDまたはパスワードが間違っています")
 
-#認証済みかどうか確認
+
+# 認証済みかどうか確認
 if "authenticated" not in st.session_state:
     st.session_state["authenticated"] = False
 
@@ -38,27 +47,53 @@ else:
     st.sidebar.button("ログアウト", on_click=lambda: st.session_state.update({"authenticated": False}))
 
     # メインページのコンテンツ
-    import requests
 
-    url = "https://pythonapi-egwh.onrender.com/getrolldata/"
+
+    url = "https://altxfastapi-test.onrender.com/getrolldata/"
     st.title("メインページ")
-    
-    #ユーザーの役職によって表示内容を変更
-    if st.session_state["role"] == "部長":
+    columns = ['日時', '役職', 'メッセージ']
+    # ユーザーの役職によって表示内容を変更
+    if st.session_state["role"] == "社長":
+        st.write("部長専用のコンテンツ")
+        res = requests.get(url + "sh")
+        data = res.json()
+        print(data)
+        df = pd.DataFrame(data, columns=columns)
+        #df = df.drop(df.columns[-5:], axis=1)
+        st.write(df)
+        print(df)
+        # 社長向けの追加情報やアクション
+    elif st.session_state["role"] == "事業部長":
+        st.write("部長専用のコンテンツ")
+        res = requests.get(url + "jb")
+        data = res.json()
+        print(data)
+        df = pd.DataFrame(data, columns=columns)
+        #df = df.drop(df.columns[-5:], axis=1)
+        st.write(df)
+        print(df)
+        # 部長向けの追加情報やアクション    
+    elif st.session_state["role"] == "部長":
         st.write("部長専用のコンテンツ")
         res = requests.get(url + "bc")
         data = res.json()
-        st.write(data)
+        print(data)
+        df = pd.DataFrame(data, columns=columns)
+        #df = df.drop(df.columns[-5:], axis=1)
+        st.write(df)
+        print(df)
         # 部長向けの追加情報やアクション
     elif st.session_state["role"] == "課長":
         st.write("課長専用のコンテンツ")
         res = requests.get(url + "kc")
         data = res.json()
-        st.write(data)
+        df = pd.DataFrame(data, columns=columns)
+        st.write(df)
         # 課長向けの追加情報やアクション
     elif st.session_state["role"] == "GL":
         st.write("GL専用のコンテンツ")
         res = requests.get(url + "gl")
         data = res.json()
-        st.write(data)
+        df = pd.DataFrame(data, columns=columns)
+        st.write(df)
         # GL向けの追加情報やアクション
